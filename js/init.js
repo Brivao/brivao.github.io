880ae9de9628817332bd7c0631b2ce663e787499
Kevin Walter <walkev13@gmail.com>
2014-07-15 11:44:03 +0200

    Nouvelle version du site

--- js/init.js
+++ js/init.js
@@ -6,15 +6,15 @@
 
 skel.init({
 	reset: 'full',
-	breakpoints: {
-		global:		{ range: '*', href: 'css/style.css', containers: 1400, grid: { gutters: 50 } },
-		wide:		{ range: '-1680', href: 'css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
-		normal:		{ range: '-1280', href: 'css/style-normal.css', containers: 960, lockViewport: true },
-		narrow:		{ range: '-980', href: 'css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
-		narrower:	{ range: '-840', href: 'css/style-narrower.css', grid: { collapse: 1 } },
-		mobile:		{ range: '-640', href: 'css/style-mobile.css', containers: '100%', grid: { gutters: 15, collapse: 2 } }
-	}
-}, {
+    breakpoints: {
+        global:     { range: '*', href: './css/style.css', containers: 1400, grid: { gutters: 50 } },
+        wide:       { range: '-1680', href: './css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
+        normal:     { range: '-1280', href: './css/style-normal.css', containers: 960, lockViewport: true },
+        narrow:     { range: '-980', href: './css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
+        narrower:   { range: '-840', href: './css/style-narrower.css', grid: { collapse: 1 } },
+        mobile:     { range: '-640', href: './css/style-mobile.css', containers: '100%', grid: { gutters: 15, collapse: 2 } }
+    }
+}/*, {
 	layers: {
 		
 		// Transform test.
@@ -67,7 +67,7 @@ skel.init({
 				}
 			}
 	}
-});
+}*/);
 
 (function($) {
 
@@ -114,7 +114,7 @@ skel.init({
 		// to revert it back to normal styling once the user scrolls past the banner.
 		// Note: This is disabled on touch devices and whenever the 'normal' breakpoint is
 		// active (effectively disabling it on 'narrow', 'narrower', and 'mobile' as well).
-			if (!skel.vars.isTouch
+		/*	if (!skel.vars.isTouch
 			&&	$header.hasClass('alt')
 			&&	$banner.length > 0) {
 
@@ -135,7 +135,7 @@ skel.init({
 				});
 			
 			}
-			
+			*/
 	});
 
 })(jQuery);
